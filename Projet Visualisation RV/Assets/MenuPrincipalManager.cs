0d8c93615f9d1c2d732cf2d2b56d9569c9bf92e5
karaxdc <73106991+karaxdc@users.noreply.github.com>
2020-12-03 17:00:46 +0100

    Creation MenuPrincipal Script

---
+++ Projet Visualisation RV/Assets/MenuPrincipalManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    public void QuitOnClick()
+    {
+        print("1");
+        Application.Quit();
+    }
+}
