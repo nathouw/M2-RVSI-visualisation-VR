08d3195897e41c5f9e321f02902170257ce29ebb
karaxdc <73106991+karaxdc@users.noreply.github.com>
2020-12-10 17:12:16 +0100

    camera a main (part 2)

---
+++ Projet Visualisation RV/Assets/CameraStickController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStickController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject light;
+    private bool bLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<OVRGrabbable>().isGrabbed && OVRInput.GetDown(OVRInput.Button.One))
+        {
+            if(bLight)
+            {
+                bLight = false;
+                light.GetComponent<Light>().enabled = false;
+            }
+            else
+            {
+                bLight = true;
+                light.GetComponent<Light>().enabled = true;
+            }
+        }
+    }
+}
--- Projet Visualisation RV/Assets/MenuOrganeManager.cs
+++ Projet Visualisation RV/Assets/MenuOrganeManager.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class MenuOrganeManager : MonoBehaviour
 {
-    public GameObject organe;
-    public Transform reset;
+    
     public Slider scaleSlider;
 
     // Start is called before the first frame update
@@ -21,13 +20,7 @@ public class MenuOrganeManager : MonoBehaviour
         
     }
 
-    public void ResetPos()
-    {
-        
-    }
     
-    public void ScaleChange()
-    {
-        organe.transform.localScale = new Vector3(scaleSlider.value, scaleSlider.value, scaleSlider.value);
-    }
+
+    
 }
