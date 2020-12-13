4f162bd96d8fd9e2c2b2e103b8bf0cd7b6a30d34
NellyLmr <nelly_1998@hotmail.fr>
2020-12-13 03:06:20 +0100

    ScaleColorReset

--- Projet Visualisation RV/Assets/OrganeController.cs
+++ Projet Visualisation RV/Assets/OrganeController.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OrganeController : MonoBehaviour
 {
 
     private GameObject organe;
     public Transform reset;
-
+    public Slider sliderScale;
+    public Slider sliderColor;
     private bool rotate = true;
-
+    public Renderer organRenderer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,11 @@ public class OrganeController : MonoBehaviour
     void Update()
     {
 
-        if(rotate)
+        if (rotate)
         {
             this.transform.Rotate(new Vector3(0, 0, 1));
         }
-        if(this.GetComponent<OVRGrabbable>().isGrabbed && rotate)
+        if (this.GetComponent<OVRGrabbable>().isGrabbed && rotate)
         {
             rotate = false;
         }
@@ -35,13 +37,26 @@ public class OrganeController : MonoBehaviour
         organe.transform.position = reset.transform.position;
         organe.transform.rotation = reset.transform.rotation;
         organe.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+        sliderScale.value = scaleValue;
+        sliderColor.value = 0;
         rotate = true;
     }
 
-    public void ScaleChange(float scaleValue)
-    {
-        organe.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+    //public void ScaleChange(float scaleValue)
+    //{
+    //    organe.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
 
+    //}
+
+    public void ScaleChange()
+    {
+        gameObject.transform.localScale = Vector3.one * sliderScale.value;
     }
+
+    public void ColorSlide()
+    {
+        organRenderer.material.color = Color.Lerp(Color.red, Color.green ,sliderColor.value);
+    }
+
 
 }
